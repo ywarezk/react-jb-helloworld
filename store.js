b6d90d4bd197c4692d1fcf7661100dd643e886d8
ywarezk <ywarezk@gmail.com>
2016-06-14 11:05:52 +0300

    finished with the store file

---
+++ store.js
@@ -0,0 +1,13 @@
+/**
+ * Our app store is defined here
+ *
+ * Created June 14th, 2016
+ * @author: ywarezk | nerdeez.com
+ * @version: 1.0
+ * @copyright: nerdeez.com
+ */
+
+
+var store = Redux.createStore(reducer);
+
+console.log(store.getState());

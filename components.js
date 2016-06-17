11b7f9639e3994bcfb719a6ae5e412f457960dcc
ywarezk <ywarezk@gmail.com>
2016-06-17 23:27:56 +0300

    finished with the search form

---
+++ components.js
@@ -0,0 +1,41 @@
+/**
+ * Prints hello world on the screen
+ *
+ * Created June 8th, 2016
+ * @author: Yariv Katz
+ * @version: 1.0
+ * @copyright: nerdeez.com
+ */
+
+
+class SearchForm extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.searchSubmitted = this.searchSubmitted.bind(this);
+  }
+
+
+  searchSubmitted(event) {
+    let searchString = this.refs.searchString.value;
+    alert(searchString);
+    event.preventDefault();
+  }
+
+  render(){
+    return (
+      <div>
+        <form onSubmit={this.searchSubmitted}>
+          <input type="text" ref="searchString" />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
+}
+
+
+ReactDOM.render(
+  <SearchForm />,
+  document.getElementById('example')
+);
